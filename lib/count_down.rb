aa66de62ed82665bd61384f92b278f3ba25e1238
Nyaradzo Bere <nyaradzo.bere@colorado.edu>
2020-05-30 04:49:50 +0000

    Automatically backed up by Learn

--- lib/count_down.rb
+++ lib/count_down.rb
@@ -1,11 +1,11 @@
 # Write your code here
 
+magic_exit_number = 7
 count = 0
-while count < 3 do
+while count < 10 && count != magic_exit_number do
   puts "I am the #{count}, I love to count!"
-  count += 1
+  count = count + 1
 end
-
 
 
 
